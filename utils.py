33c197e774625195698ef09257c20d33e016dad8
accasio <accasio@gmail.com>
2018-01-09 19:09:12 +0000

    test

---
+++ utils.py
@@ -0,0 +1,82 @@
+import glob
+import numpy as np
+np.random.seed(1)
+import sys
+import tensorflow as tf
+from matplotlib.image import imread
+import matplotlib.pyplot as plt
+from pandas.io.parsers import read_csv
+from random import shuffle
+import cv2
+import pandas as pd
+from PIL import Image
+
+dir = "F:/Datasets/MTSD/"
+image_dir = dir + 'Detection/'
+train_filename = 'train.tfrecords'  # address to save the TFRecords file
+val_filename = 'val.tfrecords'  # address to save the TFRecords file
+test_filename = 'test.tfrecords'  # address to save the TFRecords file
+
+
+def read_file():
+    return read_csv(dir + "GT.csv", sep=';', skiprows=1, header=None, names=["filename", "xmin", "ymin", "xmax",
+                                                                      "ymax", "Sign Type", "Sign Group", "Sign Class", "TS Class", "Class ID",
+                                                                      "TS Color", "Shape", "Shape ID", "Lighting",
+                                                                      "Image Source"])
+
+
+def class_distribution(labels):
+    sign_classes, class_indices, class_counts = np.unique(labels, return_index=True, return_counts=True)
+    num_classes = sign_classes.shape[0]
+    x_tix = range(sign_classes[0], num_classes + 2)
+    bins = np.arange(num_classes + 2) - 0.5
+    fig = plt.figure(figsize=(25,8))
+    plt.style.use('fivethirtyeight')
+    ax1 = fig.add_subplot(1,1,1)
+    plt.hist(labels, bins)
+    plt.title("Number of samples per different sign type", loc='center')
+    plt.xlabel("Traffic Sign class/ids")
+    plt.ylabel("Samples ")
+    plt.xticks(np.arange(min(x_tix), max(x_tix), 1.0))
+    plt.show()
+
+
+def parse_img_addr(addrs):
+    return [image_dir + addr.replace("'", "") for addr in addrs]
+
+
+def train_test(signs, shuffle_data):
+    addrs = signs['File Name'].get_values()
+    addrs = parse_img_addr(addrs)
+    labels = signs['Class ID'].get_values()
+    # to shuffle data
+    if shuffle_data:
+        c = list(zip(addrs, labels))
+        shuffle(c)
+        addrs, labels = zip(*c)
+
+    # Divide the hata into 80% train, 20% and 20% test
+    train_addrs = addrs[0:int(0.8 * len(addrs))]
+    train_labels = labels[0:int(0.8 * len(labels))]
+    test_addrs = addrs[int(0.8 * len(addrs)):]
+    test_labels = labels[int(0.8 * len(labels)):]
+    return train_addrs, train_labels, test_addrs, test_labels
+
+
+def draw_boxes(image_name):
+    full_labels = read_file()
+    full_labels.head()
+    selected_value = full_labels[full_labels.filename == image_name]
+    img = cv2.imread(image_dir + '{}'.format(image_name))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    for index, row in selected_value.iterrows():
+        img = cv2.rectangle(img, (row['xmin'], row['ymin']), (row['xmax'], row['ymax']), (0, 255, 0), 1)
+    return img
+
+
+
+if __name__ == '__main__':
+    full_labels = read_file()
+    full_labels.head()
+    Image.fromarray(draw_boxes(full_labels['filename'].get_values()[2])).show()
+
