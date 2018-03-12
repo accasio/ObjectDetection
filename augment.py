b2e803487ce5dfc0545604e5cd4ead59a253c04a
accasio <accasio@gmail.com>
2018-03-12 15:43:34 +0000

    augmentation branch

--- augment.py
+++ augment.py
@@ -2,6 +2,7 @@ import imgaug as ia
 from imgaug import augmenters as iaa
 import cv2
 import utils
+import pandas as pd
 ia.seed(1)
 
 
@@ -15,6 +16,7 @@ def draw_before_after(bbs, image, bbs_aug, image_aug):
 
 def main():
     file = utils.read_file()
+    file2 = file.copy()
     image1 = file.iloc[0]
 
     image = cv2.imread(image1['filename'])
@@ -24,31 +26,17 @@ def main():
     ], shape=image.shape)
 
     seq = iaa.Sequential([
-        iaa.Multiply((1.2, 1.5)), # change brightness, doesn't affect BBs
+        iaa.Multiply((1.2, 1.5)),
         iaa.Affine(
             translate_px={"x": 40, "y": 60},
             scale=(0.5, 0.7)
-        ) # translate by 40/60px on x/y axis, and scale to 50-70%, affects BBs
+        )
     ])
 
-    # Make our sequence deterministic.
-    # We can now apply it to the image and then to the BBs and it will
-    # lead to the same augmentations.
-    # IMPORTANT: Call this once PER BATCH, otherwise you will always get the
-    # exactly same augmentations for every batch!
     seq_det = seq.to_deterministic()
-
-    # Augment BBs and images.
-    # As we only have one image and list of BBs, we use
-    # [image] and [bbs] to turn both into lists (batches) for the
-    # functions and then [0] to reverse that. In a real experiment, your
-    # variables would likely already be lists.
     image_aug = seq_det.augment_images([image])[0]
     bbs_aug = seq_det.augment_bounding_boxes([bbs])[0]
-
-    # print coordinates before/after augmentation (see below)
-    # use .x1_int, .y_int, ... to get integer coordinates
-
+    bbs_aug = bbs_aug.remove_out_of_image().cut_out_of_image()
     for i in range(len(bbs.bounding_boxes)):
         before = bbs.bounding_boxes[i]
         after = bbs_aug.bounding_boxes[i]
@@ -57,11 +45,20 @@ def main():
             before.x1, before.y1, before.x2, before.y2,
             after.x1, after.y1, after.x2, after.y2)
         )
-        new_df = pd.DataFrame([[5, 6], [7, 8]], columns=list('AB')
+        new_name = image1['filename'] + 'generated'
 
-    # image with BBs before/after augmentation (shown below)
+        df = {
+            'filename': new_name,
+            'xmin': after.x1,
+            'ymin': after.y1,
+            'xmax': after.x2,
+            'ymax': after.y2,
+        }
 
+        file2.append(pd.DataFrame(data=df, index=[0]))
 
+    # file2.to_csv(utils.dir + 'test.txt', sep=',', index=False)
+    draw_before_after()
 
 if __name__ == '__main__':
     main()
